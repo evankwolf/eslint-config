e24c123bbaca1159574436bf8dc2cf8e278ec2c0
evankwolf <452367115@qq.com>
2023-07-24 23:05:14 +0800

    refactor: update eslint rules

--- packages/eslint-config-vue/index.js
+++ packages/eslint-config-vue/index.js
@@ -1,10 +1,3 @@
-const { isPackageExists } = require('local-pkg')
-
-const TS = isPackageExists('typescript')
-
-if (!TS)
-  console.warn('[@evanryuu/eslint-config] TypeScript is not installed, fallback to JS only.')
-
 module.exports = {
   overrides: [
     {
@@ -13,37 +6,28 @@ module.exports = {
       parserOptions: {
         parser: '@typescript-eslint/parser',
       },
-      rules: {
-        'no-unused-vars': 'off',
-        'no-undef': 'off',
-        ...(TS
-          ? { '@typescript-eslint/no-unused-vars': 'off' }
-          : null),
-      },
     },
   ],
-  plugins: ['css-import-order'],
+  plugins: ['css-import-order', 'import'],
   extends: [
     'plugin:vue/vue3-recommended',
     'plugin:css-import-order/recommended',
-    TS
-      ? '@evanryuu/eslint-config-ts'
-      : '@evanryuu/eslint-config-basic',
   ],
   rules: {
     'import/order': [
       'error',
       {
-        groups: [
+        'groups': [
           'builtin',
           'external',
           'internal',
           'sibling',
           'index',
           'object',
+          'unknown',
           'type',
         ],
-        pathGroups: [
+        'pathGroups': [
           {
             pattern: '*.css',
             group: 'type',
@@ -51,7 +35,7 @@ module.exports = {
           },
         ],
         'newlines-between': 'always',
-        alphabetize: {
+        'alphabetize': {
           order: 'asc',
           caseInsensitive: true,
         },
--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -2,15 +2,23 @@ const { isPackageExists } = require('local-pkg')
 
 const IS_VUE = isPackageExists('vue')
 const IS_REACT = isPackageExists('react')
+const IS_TS = isPackageExists('typescript')
+
+if (!IS_TS)
+  console.warn('[@evanryuu/eslint-config] TypeScript is not installed, fallback to JS only.')
 
 const frameExtends = [
   IS_VUE ? '@evanryuu/eslint-config-vue' : null,
-  IS_REACT ? '@evanryuu/eslint-config-react' : null
+  IS_REACT ? '@evanryuu/eslint-config-react' : null,
 ]
 
+const typeExtends = [
+  IS_TS ? '@evanryuu/eslint-config-ts' : '@evanryuu/eslint-config-basic',
+]
 
 module.exports = {
   extends: [
-    ...frameExtends.filter(ext => ext !== null)
+    ...frameExtends.filter(ext => ext !== null),
+    ...typeExtends,
   ],
 }
